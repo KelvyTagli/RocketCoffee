b96f142c91c05719d55a9bf264f17a3c0a4fd62d
Kelvy Tagli <kelvytagliacolli@hotmail.com.br>
2023-09-22 01:35:06 -0300

    :computer: :dart:

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const deskTop = document.querySelector('.desktop');
 const mobile = document.querySelector('.hide');
-const menuMobile = document.querySelector('.hide-mobile');
-const menu = document.querySelector('#menu-buguer-open');
+const menuMobile = document.querySelector('.hide-menu');
+const menu = document.querySelector('#menu-buguer-mobile');
 const img = document.querySelector('#buguer-open');
+const widthWindow = window.innerWidth;
 var count = 0;
 
 window.addEventListener("resize", () => {
@@ -26,16 +27,18 @@ if (widthWindow < 672) {
 } else if (widthWindow > 600){
     deskTop.classList.remove('hide');
     mobile.classList.add('hide');
-
 }
 
 
 menu.addEventListener('click', (e) => {
     if (count == 0) {
         img.setAttribute('src', './assets/menu-buguer-close.svg');
+        menuMobile.classList.add('menu-mobile');
         count += 1;
     } else {
         img.setAttribute('src', './assets/menu-buguer-open.svg');
+        menuMobile.classList.remove('menu-mobile');
+        menuMobile.classList.add('hide-menu');
         count -= 1;
     }
     e.preventDefault();
